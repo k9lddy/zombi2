3f9ab91112ac1b9262e3159f2d7791a1fde18b31
k9lddy <151925648+k9lddy@users.noreply.github.com>
2023-11-24 16:40:29 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1700825673,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ПлиточныйФон.png",
+		"images/герой-sheet0.png",
+		"images/зомби-sheet0.png",
+		"images/пуля-sheet0.png",
+		"images/взрыв-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
